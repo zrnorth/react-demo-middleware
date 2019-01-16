7eaaabc230357ff5c64e3b419a7da78974bb1438
Zach North <zrnorth@gmail.com>
2019-01-16 15:38:36 -0800

    Initial dummy app and some tests

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { mount } from "enzyme";
+import CommentBox from "components/CommentBox";
+
+let wrapped;
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it("shows a text area and a button", () => {
+  expect(wrapped.find("textarea").length).toEqual(1);
+  expect(wrapped.find("button").length).toEqual(1);
+});
+
+describe("the textarea", () => {
+  const comment = "new comment";
+
+  beforeEach(() => {
+    wrapped.find("textarea").simulate("change", {
+      target: { value: comment } // this gets merged into the event obj in the handler
+    });
+    wrapped.update();
+  });
+
+  it("lets users enter input", () => {
+    expect(wrapped.find("textarea").prop("value")).toEqual(comment);
+  });
+
+  it("empties the text area when submitted", () => {
+    wrapped.find("form").simulate("submit");
+    wrapped.update();
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+  });
+});
